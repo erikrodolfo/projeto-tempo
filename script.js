1334feaa9ddce218e417f0b36d74be7ee64b2804
Erik Rodolfo <128737807+erikrodolfo@users.noreply.github.com>
2023-05-07 21:11:29 -0300

    update script.js

--- script.js
+++ script.js
@@ -7,29 +7,34 @@ function calcTempo() {
 		//meses vividos
 		let diferencaMeses = dataAtual - dataDeNascimento
 		let mesesVividos = Math.floor(diferencaMeses / (1000 * 60 * 60 * 24 * 30.44))
+		mes.innerHTML = `${mesesVividos.toLocaleString()} meses`
 		
 		//semanas vividas
 		let diferencaSem = dataAtual - dataDeNascimento
 		let semanasVividas = Math.floor(diferencaSem / (1000 * 60 * 60 * 24 * 7))
+		sem.innerHTML = `${semanasVividas.toLocaleString()} semanas`
 		
 		//dias vividos
 		let diferencaDias = dataAtual - dataDeNascimento
 		let diasVividos = Math.floor(diferencaDias / (1000 * 60 * 60 * 24))
+		dia.innerHTML = `${diasVividos.toLocaleString()} dias`
 		
 		//horas vividas
 		let diferencaHoras = dataAtual.getTime() - dataDeNascimento.getTime()
 		let horasVividas = Math.floor(diferencaHoras / (1000 * 60 * 60))
+		hora.innerHTML = `${horasVividas.toLocaleString()} horas`
 		
 		//minutos vividos
 		let diferencaMin = dataAtual.getTime() - dataDeNascimento.getTime()
 		let minutosVividos = Math.floor(diferencaMin / (1000 * 60))
+		min.innerHTML = `${minutosVividos.toLocaleString()} minutos`
 		
 		//segundos vividos
 		let diferencaSeg = dataAtual.getTime() - dataDeNascimento.getTime()
 		let segundosVividos = Math.floor(diferencaSeg / (1000))
-		
-		setInterval(calcTempo, 1000)
-		
+		seg.innerHTML = `${segundosVividos.toLocaleString()} segundos`
+		console.log(segundosVividos)
+		/*
 		res.innerHTML = 
 		`<strong>
 		  ${mesesVividos.toLocaleString()} Meses<br>
@@ -39,4 +44,7 @@ function calcTempo() {
 			${minutosVividos.toLocaleString()} minutos<br> 
 			${segundosVividos.toLocaleString()} segundos
 			</strong>`
+			*/
+		
+		setInterval(calcTempo, 1000)
 	}
