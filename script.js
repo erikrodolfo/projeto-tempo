4734363654f3c4bcc4ecff9e06fd8c34632f38bc
Erik Rodolfo <128737807+erikrodolfo@users.noreply.github.com>
2023-05-08 22:03:54 -0300

    update script.js

--- script.js
+++ script.js
@@ -5,37 +5,37 @@ function calcTempo() {
 		let dataAtual = new Date()
 		
 		//data completa
-		nasc.innerHTML = `${data.value}`
+		//nasc.innerHTML = `${data.value}`
 		
 		//meses vividos
 		let diferencaMeses = dataAtual - dataDeNascimento
 		let mesesVividos = Math.floor(diferencaMeses / (1000 * 60 * 60 * 24 * 30.44))
-		mes.innerHTML = `${mesesVividos.toLocaleString()} meses`
+		mes.innerHTML = `${mesesVividos.toLocaleString()} Meses`
 		
 		//semanas vividas
 		let diferencaSem = dataAtual - dataDeNascimento
 		let semanasVividas = Math.floor(diferencaSem / (1000 * 60 * 60 * 24 * 7))
-		sem.innerHTML = `${semanasVividas.toLocaleString()} semanas`
+		sem.innerHTML = `${semanasVividas.toLocaleString()} Semanas`
 		
 		//dias vividos
 		let diferencaDias = dataAtual - dataDeNascimento
 		let diasVividos = Math.floor(diferencaDias / (1000 * 60 * 60 * 24))
-		dia.innerHTML = `${diasVividos.toLocaleString()} dias`
+		dia.innerHTML = `${diasVividos.toLocaleString()} Dias`
 		
 		//horas vividas
 		let diferencaHoras = dataAtual.getTime() - dataDeNascimento.getTime()
 		let horasVividas = Math.floor(diferencaHoras / (1000 * 60 * 60))
-		hora.innerHTML = `${horasVividas.toLocaleString()} horas`
+		hora.innerHTML = `${horasVividas.toLocaleString()} Horas`
 		
 		//minutos vividos
 		let diferencaMin = dataAtual.getTime() - dataDeNascimento.getTime()
 		let minutosVividos = Math.floor(diferencaMin / (1000 * 60))
-		min.innerHTML = `${minutosVividos.toLocaleString()} minutos`
+		min.innerHTML = `${minutosVividos.toLocaleString()} Minutos`
 		
 		//segundos vividos
 		let diferencaSeg = dataAtual.getTime() - dataDeNascimento.getTime()
 		let segundosVividos = Math.floor(diferencaSeg / (1000))
-		seg.innerHTML = `${segundosVividos.toLocaleString()} segundos`
+		seg.innerHTML = `${segundosVividos.toLocaleString()} Segundos`
 		console.log(segundosVividos)
 		/*
 		res.innerHTML = 
